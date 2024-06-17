28c399b83941a9229fda2c77184dc55dbbf6849f
phanhuutin30504 <phanhuutin3052004@gmail.com>
2024-06-17 18:20:40 +0700

    source

---
+++ database/migrations/2024_06_17_035847_create_theaters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theaters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cinema_id');
+            $table->string('theater_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('theaters');
+    }
+};
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('welcome')
+<title>@yield('title', 'Admin Dashboard')</title>
+@section('content')
+@include('admin.layouts.sidebar')
+ heloo
+@endsection
